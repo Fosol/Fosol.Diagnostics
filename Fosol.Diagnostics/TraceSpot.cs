118f2d97e5f50ff78ddc97a7a9e261dcb17dc80c
Jeremy Foster <jeremymfoster@hotmail.com>
2013-10-03 14:14:15 -0700

    Additional comments.

--- Fosol.Diagnostics/TraceSpot.cs
+++ Fosol.Diagnostics/TraceSpot.cs
@@ -6,12 +6,22 @@ using System.Text;
 namespace Fosol.Diagnostics
 {
     /// <summary>
-    /// TraceSpot provides a way to identify the position of a TraceEvent
+    /// TraceSpot provides a way to identify the position of a TraceEvent.
+    /// Specifically this enum provides a way to determine if a TraceEvent is a header or footer.
     /// </summary>
     enum TraceSpot
     {
+        /// <summary>
+        /// A default message.
+        /// </summary>
         Message = 0,
+        /// <summary>
+        /// A header message.
+        /// </summary>
         Header = 1,
+        /// <summary>
+        /// A footer message.
+        /// </summary>
         Footer = 2
     }
 }
